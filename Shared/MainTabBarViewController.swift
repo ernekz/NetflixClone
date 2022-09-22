c1f6646d3478bbad801eb3f9df1cbc8743befddc
Ernestas <kazinevicius@gmail.com>
2022-09-22 18:52:22 +0200

    Initial commit

---
+++ Shared/MainTabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Shared
+//
+//  Created by Ernestas Kazinevicius on 2022-09-21.
+//
+import CoreData
+import UIKit
+
+class MainTabBarViewController: UITableViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
